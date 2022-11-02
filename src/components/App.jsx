57bc8f6eae1f759821bf34b0c9480fedc9a06a75
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-02 12:29:38 +0300

    start files

---
+++ src/components/App.jsx
@@ -0,0 +1,82 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid/non-secure';
+import { Box } from './Box/Box';
+import { H1, H2 } from './Title/Title';
+import { ContactList } from './ContactList/ContactList';
+import Phonebook from './Phonebook/Phonebook';
+import { Filter } from './Filter/Filter';
+import { P } from './Style/Element.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  reset = () => {
+    this.setState({ filter: '' });
+  };
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  formSubmit = data => {
+    const id = nanoid();
+    if (
+      this.state.contacts.filter(contact => contact.name === data.name).length >
+      0
+    ) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+    this.setState({
+      contacts: [
+        ...this.state.contacts,
+        {
+          name: data.name,
+          number: data.number,
+          id: id,
+        },
+      ],
+    });
+  };
+
+  onClickDelete = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+    this.reset();
+  };
+
+  render() {
+    const filterValueNormolize = this.state.filter.toLowerCase();
+    const contactsToShow = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterValueNormolize)
+    );
+    return (
+      <Box as="main" px={5}>
+        <H1 title="Phonebook" />
+        <Phonebook onChange={this.handleChange} onSubmit={this.formSubmit} />
+        {this.state.contacts.length > 0 ? (
+          <>
+            <H2 title="Contacts" />
+            <Filter
+              title="Find contact by name"
+              onChange={this.handleChange}
+              value={this.state.filter}
+            />
+            <ContactList
+              //contacts={this.state.contacts}
+              contacts={contactsToShow}
+              onClickDelete={this.onClickDelete}
+            />
+          </>
+        ) : (
+          <P>Phonebook is empty</P>
+        )}
+      </Box>
+    );
+  }
+}
